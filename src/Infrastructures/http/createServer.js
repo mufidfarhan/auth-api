e36b34e0cd4beef9ee9aef3a650c88c632c6df92
mufidfarhan <mufidfarhan68@gmail.com>
2025-02-02 16:36:11 +0700

    pass say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -23,6 +23,14 @@ const createServer = async (container) => {
     },
   ]);
 
+  server.route({
+    method: 'GET',
+    paht: '/',
+    handler: () => ({
+      value: 'Hello world!',
+    }),
+  });
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
